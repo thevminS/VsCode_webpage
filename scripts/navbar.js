1f5198d17198082074feac773db05d161dea428a
thevmin <owtsanhida@gmail.com>
2023-02-25 09:56:33 +0530

    linked tab and navbar

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -8,7 +8,20 @@ function setNavActive(link){
     for(let i = 0; i< svg_grp.length;i++){
         svg_grp[i].classList.remove('active')
     } 
-    
     link.classList.add('active');
     link.querySelector('svg').classList.add('active');
+}
+
+function onNavClick(link, name){
+    setNavActive(link);
+    if(name =='home'){
+        const tab1 = document.getElementById('tab1');
+        setTabActive(tab1);
+    }else if(name =='project'){
+        const tab2 = document.getElementById('tab2');
+        setTabActive(tab2);
+    }else if(name == 'contact'){
+        const tab3 = document.getElementById('tab3');
+        setTabActive(tab3);
+    }
 }
--- scripts/tab.js
+++ scripts/tab.js
@@ -5,3 +5,16 @@ function setTabActive(link){
     }
     link.classList.add('active');
 }
+
+function onTabClick(name){
+    if(name =='home'){
+        const nav1 = document.getElementById('nav_home');
+        onNavClick(nav1, name)
+    }else if(name =='project'){
+        const nav2 = document.getElementById('nav_project');
+        onNavClick(nav2, name);
+    }else if(name == 'contact'){
+        const nav3 = document.getElementById('nav_contact');
+        onNavClick(nav3, name);
+    }
+}
